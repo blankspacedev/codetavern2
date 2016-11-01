e66c8b6b22a2fb8ea68b2ff66d534fbe324f914c
jcozar87 <javier.cozar@uclm.es>
2016-11-01 12:58:11 +0100

    Uso de una base de datos

--- helloTommy.py
+++ helloTommy.py
@@ -1,10 +1,40 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+'mysql://blankspace:BSdev2016!@localhost/codetavern2'
+
+db = SQLAlchemy(app)
+
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(32), nullable = False, unique = True)
+    pedidos = db.Column(db.Integer, default = 0)
+
+def populate():
+    #Crea la base de datos. Si ya está creada, no hace nada
+    #(¡aunque la hayamos modificado!)
+    db.create_all()
+
+    if Cliente.query.filter_by(nombre='Javi').first() is None:
+        javi = Cliente(nombre="Javi", pedidos = 3)
+        #Crea un contexto para guardar para usar la variable de entorno session.
+        app_ctx = app.app_context()
+        app_ctx.push()
+        db.session.add(javi)
+        db.session.commit()
+        app_ctx.pop()
+
 @app.route('/')
 def index():
-    return "<strong>Hello Tommy!</strong>"
+    javi = Cliente.query.filter_by(nombre='Javi').first()
+    return "<strong>Hello Tommy!</strong>"+\
+    "<br>"+\
+    "%s se ha pedido %d cervezas." % (javi.nombre, javi.pedidos)
 
 if __name__ == '__main__':
+    populate()
     app.run()
