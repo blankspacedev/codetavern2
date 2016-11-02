9592ce8fe9cb8596d2825b7850d3ab54f2b92bb4
jcozar87 <javier.cozar@uclm.es>
2016-11-02 11:51:46 +0100

    Estructuración del proyecto

--- helloTommy.py
+++ helloTommy.py
@@ -1,20 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-'mysql://blankspace:BSdev2016!@localhost/codetavern2'
-
-db = SQLAlchemy(app)
-
-@app.route('/')
-def index():
-    javi = Cliente.query.filter_by(nombre='Javi').first()
-    return "<strong>Hello Tommy!</strong>"+\
-    "<br>"+\
-    "%s se ha pedido %d cervezas." % (javi.nombre, javi.pedidos)
+from app import manager
 
 if __name__ == '__main__':
-    from model import Cliente
-    app.run()
+    manager.run()
